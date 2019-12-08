6e4f1c21c0ee464b3ad1fd172521d42e60043838
HardreaM <46752979+HardreaM@users.noreply.github.com>
2019-12-08 19:26:40 +0500

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+"# m-tele-bot-y" 
---
+++ simple_tele_bot.py
@@ -0,0 +1,33 @@
+import telebot
+import pyowm
+
+owm = pyowm.OWM('', language="ru")
+bot = telebot.TeleBot('')
+
+temp_list = []
+
+keyboard2 = telebot.types.ReplyKeyboardMarkup()
+keyboard2.row('/weather', '/help')
+
+@bot.message_handler(commands=['help'])
+def help(message):
+	bot.send_message(message.chat.id,
+	 'Чтобы узнать погоду используйте команду /weather и следуйте инструкции')
+
+@bot.message_handler(commands=['weather'])
+def send_welcome(message):
+	msg = bot.reply_to(message, "Здравствуйте, чтобы узнать погоду, укажите свой город.")
+	bot.register_next_step_handler(msg, weather_forecast)
+
+
+def weather_forecast(message):
+    observation = owm.weather_at_place(message.text)
+    w = observation.get_weather()
+    temp = w.get_temperature('celsius')["temp"]
+
+    answer = 'В городе ' + message.text + ' сейчас ' + w.get_detailed_status() + '\n'
+    answer += 'Температура сейчас в районе ' + str(temp)
+
+    bot.send_message(message.chat.id, answer)
+
+bot.polling(none_stop='true')
